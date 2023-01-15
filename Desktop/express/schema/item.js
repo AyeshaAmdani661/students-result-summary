a9439b31d27ea50a0febb46956b4b577e8b62f85
AyeshaAmdani661 <96915759+AyeshaAmdani661@users.noreply.github.com>
2023-01-15 20:30:59 +0500

    Delete Desktop/express directory
    delete

--- Desktop/express/schema/item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-let itemInfo = new mongoose.Schema({
-    id: Number,
-    title: String,
-    price: Number
-})
-
-module.exports = itemInfo = mongoose.model("Items", itemInfo);
